2c59ca4fb93e01675521fe3611539c5464d2490a
darrellfr3 <darrellfr3@gmail.com>
2020-11-25 22:00:58 -0600

    patched navbar in html,javascript, and css files. Need review over css.

---
+++ assets/js/formScript.js
@@ -0,0 +1,13 @@
+//Calls location API to gather location and print to nav bar
+var queryURL="https://api.geoapify.com/v1/ipinfo?apiKey=805c055b98f34c71807194113fb46926"
+$.ajax({url:queryURL,method:"GET"}).then(function(response){
+    //Stores user's general location info into session
+    sessionStorage.setItem("city",response.city.name);  //Grab with "sessionStorage.getItem("city")"
+    sessionStorage.setItem("state",response.state.name);    //Grab with "sessionStorage.getItem("state")"
+    sessionStorage.setItem("country",response.country.name);  //Grab with "sessionStorage.getItem("country")"
+
+    //Grabs location information from session storage
+    $(".navLocation").text(sessionStorage.getItem("city")+", "+sessionStorage.getItem("state")+" | ");
+})
+
+// End of Geoapify API Code //
